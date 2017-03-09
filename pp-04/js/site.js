552db87ba213821ed4e6e0ac3500f5a4f0fdbf4c
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-03-08 20:29:55 -0600

    added webfontloader to js

--- pp-04/js/site.js
+++ pp-04/js/site.js
@@ -6,3 +6,9 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+// adding webfontloader
+    WebFont.load({
+      google: {
+        families:['Gill Sans', 'Gill Sans MT']
+      }
+    });
